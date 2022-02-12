ca32680bab20e1501a8870e9589f1f66867dbb95
KatkaNo <99351093+KatkaNo@users.noreply.github.com>
2022-02-12 16:04:55 +0300

    Add files via upload

---
+++ homework_1/task_10.py
@@ -0,0 +1,6 @@
+user = input('Введите пользователя: ')
+new_file = input('Введите имя файла: ')
+a = 'C:/'
+b = '/docs/folder/'
+c = '.txt'
+print(a+user+b+new_file+c)
---
+++ homework_1/task_9.py
@@ -0,0 +1,3 @@
+from_where = input('Введите город вылета: ')
+to_where = input('Введите место назвачения: ')
+print(from_where,'-',to_where)
